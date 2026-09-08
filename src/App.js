03329e84d789660277a74427581299c20a8d17e9
ddeval <ddeval81@gmail.com>
2026-09-08 12:12:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Maman Marie I love you so much and I will always love you. You are the best mom in the world and I am so grateful for everything you do for me.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
